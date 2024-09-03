39f1d6d466b3e59be3778ec952722ee0fde29399
Roberto Carlos Callisaya Mamani <calyr.software@gmail.com>
2024-09-03 12:17:37 -0400

    add manual module data

---
+++ data/build.gradle.kts
@@ -0,0 +1,9 @@
+plugins {
+    id("java-library")
+    alias(libs.plugins.jetbrains.kotlin.jvm)
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_17
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,4 +21,5 @@ dependencyResolutionManagement {
 
 rootProject.name = "Franky App"
 include(":app")
+include(":data")
  
