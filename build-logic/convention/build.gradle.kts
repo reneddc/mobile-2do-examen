dfdaad5a23bc0e52b001adb57355a1ad4aa857b1
Roberto Carlos Callisaya Mamani <calyr.software@gmail.com>
2024-09-07 17:05:01 -0400

    created build-logic/convention plugin

---
+++ build-logic/convention/build.gradle.kts
@@ -0,0 +1,40 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    `kotlin-dsl`
+}
+
+group = "com.calyr.buildlogic"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+tasks.withType<KotlinCompile>().configureEach {
+    kotlinOptions {
+        jvmTarget =  JavaVersion.VERSION_17.toString()
+    }
+}
+
+tasks {
+    validatePlugins {
+        enableStricterValidation = true
+        failOnWarning = true
+    }
+}
+
+dependencies {
+    compileOnly(libs.android.gradlePlugin)
+    compileOnly(libs.android.tools.common)
+    compileOnly(libs.kotlin.gradlePlugin)
+}
+
+gradlePlugin {
+    plugins {
+        register( "androidLibrary") {
+            id = "ucb.android.library"
+            implementationClass = "AndroidLibraryConventionPlugin"
+        }
+    }
+}
---
+++ build-logic/settings.gradle.kts
@@ -0,0 +1,18 @@
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+
+    versionCatalogs {
+        create("libs") {
+            from(files("../gradle/libs.versions.toml"))
+        }
+    }
+}
+
+
+rootProject.name = "build-logic"
+
+include(":convention")
