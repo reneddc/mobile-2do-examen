73e9ac69894bc49815646afadc7757c21d074a54
Roberto Carlos Callisaya Mamani <calyr.software@gmail.com>
2024-09-07 18:38:32 -0400

    added core:network and core:ui module with ucb.android.library

---
+++ core/ui/build.gradle.kts
@@ -0,0 +1,9 @@
+plugins {
+    alias(libs.plugins.jetbrains.kotlin.android) //kotlin
+    alias(libs.plugins.ucb.android.library)
+}
+
+
+android {
+    namespace = "com.calyr.ui"
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -24,4 +24,7 @@ rootProject.name = "Franky App"
 include(":app")
 include(":data")
 include(":framework")
+include(":core")
+include(":core:network")
+include(":core:ui")
  
